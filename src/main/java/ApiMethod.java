e5b61a475432b0f5f95e08677ae67981aefa2f0f
Gustavo Huanca <gustavozha@gmail.com>
2021-06-15 17:35:20 -0400

    Add files implement in pivotal to project

---
+++ src/main/java/ApiMethod.java
@@ -0,0 +1,16 @@
+public enum ApiMethod {
+    GET("GET"),
+    POST("POST"),
+    DELETE("DELETE"),
+    PUT("PUT");
+
+    private String name;
+
+    ApiMethod(String name){
+        this.name=name;
+    }
+
+    public String toName(){
+        return name;
+    }
+}
