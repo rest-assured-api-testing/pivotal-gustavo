691cf2c289dcac19492facb28f42534da15c93de
Gustavo Huanca <gustavozha@gmail.com>
2021-06-18 15:31:44 -0400

    Add  workspace with its enpoint, test and schema

--- src/main/java/api/ParametersDefault.java
+++ src/main/java/api/ParametersDefault.java
@@ -8,7 +8,10 @@ public class ParametersDefault {
     public static String PROJECT_ID="projectId";
     public static String LABEL_ID="labelsId";
     public static String STORY_ID="storiesId";
+    public static String WORKSPACE_ID="workspaceId";
 
     public static String END_POINT_STORY="projects/{projectId}/stories/";
-    public static String END_POINT_STORY_MODIFY="projects/{projectId}/stories/{storiesId}";
+    public static String END_POINT_STORY_TO_MODIFY ="projects/{projectId}/stories/{storiesId}";
+    public static String END_POINT_WORKSPACE="my/workspaces";
+    public static String END_POINT_WORKSPACE_TO_MODIFY ="my/workspaces/{workspaceId}";
 }
